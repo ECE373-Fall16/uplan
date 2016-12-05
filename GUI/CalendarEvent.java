50dfd3c0fec8765834e5bf13e8925991ee891ffe
menziej <menziej@gmail.com>
2016-12-05 12:26:19 -0500

    updating branch, Homepage almost completed and Login page is getting null pointer error but graphic is available

---
+++ GUI/CalendarEvent.java
@@ -0,0 +1,53 @@
+import java.util.Date;
+
+public class CalendarEvent{
+    
+    private String name;
+    private Date starttime;
+    private Date endtime;
+    private String location;
+    
+    public CalendarEvent(String name1, Date start, Date end, String loc){
+        name = name1;
+        starttime = start;
+        endtime = end;
+        location = loc;
+    }
+    
+    public String toString(){
+        return "Calendar: " + name + " " + starttime + " " + endtime + " " + location;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name1){
+        name = name1;
+    }
+    
+    public Date getStartTime(){
+        return starttime;
+    }
+    
+    public void setStartTime(Date time){
+        starttime = time;
+    }
+    
+    public Date getEndTime(){
+        return endtime;
+    }
+    
+    public void setEndTime(Date time){
+        endtime = time;
+    }
+    
+    public String getLocation(){
+        return location;
+    }
+    
+    public void setLocation(String loc){
+        location = loc;
+    }
+
+}
--- GUI/Test.java
+++ GUI/Test.java
@@ -1,6 +1,11 @@
+
 public class Test{
 	public static void main(String[] args){
+		LoginPage a = new LoginPage();
 		HomePage s = new HomePage();
-		s.DisplayHomePage();
+		AddAssignment b = new AddAssignment();
+		String login = a.LoginUser();
+		System.out.println(login);
+		//s.DisplayCalendarEvents();
 	}
 }
