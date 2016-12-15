0b98c22cd1f878561db65e99d74b94cbbf250bfd
sjlucey <sjlucey@outlook.com>
2016-12-15 01:30:39 +0000

    Adding v1.0/code folder

---
+++ v1.0/code/testAddevent.java
@@ -0,0 +1,36 @@
+public class testAddevent{   
+public static void main(String[] args) {
+	
+    	String username = args[0];	
+    	String password = args[1];
+    	String email = args[2];
+    	String fullname = args[3];
+    	String bedtime = args[4];
+    	String waketime = args[5];
+		String event = args[6];	
+        String day = args[7];
+        String startDay = args[8];
+        String startHour = args[9];
+        String endDay = args[10];
+        String endHour = args[11];
+        String location = args[12];
+        		
+        Print toScreen = new Print();
+        toScreen.setEvent(event);
+        toScreen.setDay(day);
+        toScreen.setstartDay(startDay);
+        toScreen.setstartHour(startHour);
+        toScreen.setendDay(endDay);
+        toScreen.setendHour(endHour);
+        toScreen.setLocation(location);
+        toScreen.setUsername(username);
+        toScreen.setPassword(password);
+        toScreen.setEmail(email);
+        toScreen.setFullname(fullname);
+        toScreen.setBedtime(bedtime);
+        toScreen.setWaketime(waketime);
+        
+        toScreen.printAddEvent();
+        
+     }
+}
---
+++ v1.0/code/testUpdateassign.java
@@ -0,0 +1,28 @@
+public class testUpdateassign{
+	public static void main(String[] args){
+		
+		 	String username = args[0];	
+	        String password = args[1];
+	        String email = args[2];
+	        String fullname = args[3];
+	        String bedtime = args[4];
+	        String waketime = args[5];
+	        String assignment = args[6];
+	        String type = args[7];
+	        String newName = args[8];
+	        
+	        Print toScreen = new Print();
+	        toScreen.setUsername(username);
+	        toScreen.setPassword(password);
+	        toScreen.setEmail(email);
+	        toScreen.setFullname(fullname);
+	        toScreen.setBedtime(bedtime);
+	        toScreen.setWaketime(waketime);
+	        toScreen.setAssignment(assignment);
+	        toScreen.setType(type);
+	        toScreen.setnewName(newName);
+	        
+	        toScreen.printUpdateAssign();
+	        
+	}
+}
